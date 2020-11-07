4608ce0367732d12432f250be050f39611a9adf2
klewis <kaleb.lewis@gmail.com>
2020-11-07 19:58:17 +0000

    Update homepage.html, base.html, prud.py, and server.py to enable user authentication and account creation

--- crud.py
+++ crud.py
@@ -1,1 +1,87 @@
 """CRUD operations."""
+
+from model import db, User, Preference, QueryHistory, connect_to_db
+import datetime
+
+
+def create_user(name, email, password):
+    """Create and return a new user.
+
+    >>> create_user('jane', 'jane.doe@email.com', 'password1')
+    # TODO: update docstring
+
+    >>> create_user('', 'jane.doe@email.com', 'password1')
+    # TODO: update docstring
+
+    >>> create_user('jane', '', 'password1')
+    # TODO: update docstring
+
+    >>> create_user('jane', 'jane.doe@email.com', '')
+    # TODO: update docstring
+
+    user cannot pass a name that's other than alpha chars
+    >>> create_user('1', 'jane.doe@email.com', 'password1')
+    # TODO: update docstring
+
+    >>> create_user('@', 'jane.doe@email.com', 'password1')
+    # TODO: update docstring
+
+    user cannot pass an email that's not an email
+    >>> create_user('jane', 'jane.doe', 'password1')
+    # TODO: update docstring
+
+    >>> create_user('jane', 'janedoe@emailcom', 'password1')
+    # TODO: update docstring
+
+    >>> create_user('jane', '@.', 'password1')
+    # TODO: update docstring
+
+    name cannot be > 50 chars:
+    >>> create_user('jane12345678901234156789012345678901234567890123415678901234567890123456789012345', 'jane.doe@email.com', 'password1')
+    # TODO: update docstring
+
+    email cannot be > 80 chars:
+    >>> create_user('jane', 'jane.doe@email.com123456789012341567890123456789012345678901234156789012345678901', 'password1')
+    # TODO: update docstring
+
+    password cannot be > 20 chars:
+    >>> create_user('jane', 'jane.doe@email.com', 'password1234567890123')
+    # TODO: update docstring
+
+    """
+    
+    user = User(fname=name, 
+                email=email, 
+                password=password,
+                user_since = datetime.datetime.now())
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
+
+
+def get_all_users():
+    """Return all users."""
+
+    return User.query.all()
+
+
+def get_user_by_id(id):
+    """Return a user by primary key.
+    
+    >>> get_user_by_id(1)
+    <User id=1 email=jane.doe@email.com>
+    """
+
+    return User.query.get(id)
+
+
+def get_user_by_email(email):
+
+    return User.query.filter(User.email == email).first()
+
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 """Server for online viewing recommendations app."""
 
-# import crud
-from flask import (Flask, render_template, request, flash, session, redirect)
+import crud
 from model import connect_to_db
+import datetime
+from flask import (Flask, render_template, request, flash, session, redirect)
 from jinja2 import StrictUndefined
+
 
 app = Flask(__name__)
 app.secret_key = "dev"
@@ -14,7 +16,107 @@ app.jinja_env.undefined = StrictUndefined
 def homepage():
     """View homepage."""
 
-    return render_template('homepage.html')
+    try:
+        session['name']
+        return render_template("homepage.html")                             
+    except KeyError:
+        return render_template("homepage.html")
+
+
+@app.route('/users')
+def all_users():
+    """View all users."""
+
+    users = crud.get_all_users()
+
+    return render_template('all_users.html', users=users)
+
+
+@app.route('/user')                                                             #@maybe render a more unique route? ('/users/<id>')
+def show_user(id):
+    """Show details on a particular user."""
+
+    user = crud.get_user_by_id(id)
+
+    return render_template('user.html', user=user)
+
+
+@app.route('/login', methods=['POST'])
+def user_login():
+
+    email = request.form.get('email-input')
+    password = request.form.get('password-input')
+
+    login_user = crud.get_user_by_email(email)
+
+    if login_user:
+        if login_user.password == password:
+            flash('Success')
+            session['name'] = fname
+            session['email'] = email
+            session['isNew'] = False
+            session['loggedIn'] = True    
+
+        else:
+            flash(f'Wrong password, try again')
+                                                                                # TODO password counter: session['password-counter'] = session['password-counter'] + 1
+                                                                                # flash(f'You entered an incorrect password'+{session['password-counter']}+'times(s).')
+            return redirect('/')
+
+    else:
+        flash(f'Your account was not found, please create one')
+        session['name'] = 'no-account-found-please-create-account'
+        return redirect('/')
+
+                                                                                # TODO password counter: build login lockout with auto refresh later
+                                                                                        # if session exists ['password-counter'] < 6:
+                                                                                            # (wrap all of the login code here?)
+                                                                                        # else:
+                                                                                        #     flash(f'You will be able to reload this page and try again in 5 minutes')           
+
+
+@app.route('/newuser', methods=['POST'])
+def register_user():
+    """Create new user"""
+
+    fname = request.form.get('name-input')
+    email = request.form.get('email-input')
+    password = request.form.get('password-input')
+    password2 = request.form.get('password-input-2')
+
+    new_user = crud.get_user_by_email(email)
+
+    if password != password2:
+        flash(f"The passwords you entered do not match, please try again")
+        return redirect('/')
+
+    elif new_user:
+
+        if new_user.password == password:
+            flash(f"Thanks, {fname} but you already have an account with that same password")
+            flash(f"You have successfully logged in to your account")           # // TODO fix bug msg scenario where someone gets stuck in loop if they enter existing Username/password within /newuser
+            session['name'] = fname
+            session['email'] = email
+            session['isNew'] = False
+            return redirect('/')
+
+        else:
+            flash(f"Thanks, {fname} but you already have an account with that same email")
+            flash(f"Please login to your existing account")                 
+            session['name'] = fname
+            session['email'] = email
+            session['isNew'] = False
+            return redirect('/login')
+
+    else:
+        crud.create_user(fname, email, password)
+        flash("You now have an account")
+        session['name'] = fname
+        session['email'] = email
+        session['isNew'] = True  
+        session['loggedIn'] = True                                               #this way we can display dynamic content to users who are new to the site
+                                                                                # TODO password counter: reset counter when login successful:  session['password-counter'] = 0 
+        return redirect('/')
 
 
 if __name__ == '__main__':
