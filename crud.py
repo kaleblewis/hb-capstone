8a22f7ddf917f9e8ae7ebc29e47081784fb532bc
klewis <kaleb.lewis@gmail.com>
2020-11-11 21:11:01 +0000

    Debug crud.py file and comment out all placeholder function stubs

--- crud.py
+++ crud.py
@@ -90,14 +90,14 @@ def get_user_by_email(email):
 #*#                      USER CONNECTIONS OPERATIONS                         #*#
 #*############################################################################*#
 
-def add_user_connection(requestor_id, requestee_id):                            # TODO:  what params?
+def add_user_connection(user, requestee_id):                                    # TODO:  what params?
     """Create and store a user's social network connection invitation.
 
     # TODO: update docstring with doctest
     """
 
     user_connection = Connections(                                              # TODO:  what params?
-        requestor_id = requestor_id,
+        requestor_id = (User.query.get(id)),
         requestee_id = requestee_id,
         status = "Pending",
         connection_date = datetime.datetime.now()
@@ -108,16 +108,16 @@ def add_user_connection(requestor_id, requestee_id):                            
 
     return user_connection
 
-def update_user_connection_by_id(connection_id):
-    """Update a User Connection by primary key
+# def update_user_connection_by_id(connection_id):
+#     """Update a User Connection by primary key
 
-    # TODO: update docstring with doctest
-    """
+#     # TODO: update docstring with doctest
+#     """
 
-    pass
+#     pass
 
 
-def get_connections_by_user(user_id):                                           # TODO: implement new feature later to update/"approve" pending connection requests
+def get_connections_by_user(user):                                              # TODO: implement new feature later to update/"approve" pending connection requests
     """Return all of a User's requested connections.
 
     Should return all requested connections, regardless of outcome/status.
@@ -125,39 +125,43 @@ def get_connections_by_user(user_id):                                           
     # TODO: update docstring with doctest
     """
 
-    return Connections.query.filter(Connections.requestor_id == user_id)
+    return Connections.query.filter(Connections.requestor_id == (User.query.get(id)))
 
     
-def get_pending_connections_by_user(user):                                      # TODO: implement new feature later to approve pending connection requests
-    """Return all pending connections where User is the Requestee.
+# def get_pending_connections_by_user(user):                                    # TODO: implement new feature later to approve pending connection requests
+#     """Return all pending connections where User is the Requestee.
 
-    Should return only the connections where:
-        Connections.Requestee_ID == User.ID
-        Connections.status = "Pending"
+#     Should return only the connections where:
+#         Connections.Requestee_ID == User.ID
+#         Connections.status = "Pending"
 
-    # TODO: update docstring with doctest
-    """
+#     # TODO: update docstring with doctest
+#     """
 
-    pass                                                    
+#     pass                                                    
 
 
 #*############################################################################*#
 #*#                      USER PREFERENCES OPERATIONS                         #*#
 #*############################################################################*#
 
-def add_user_preference_to_preferences():                                       # TODO:  what params?
+def add_user_preference_to_preferences(user, param_subtitle="", 
+    param_dubbing="", param_genre="", param_release_date="", param_duration="", 
+    param_total_seasons=""):                                                    # TODO:  what params?
     """Create and store a collection of User's default preferences"""
 
-    user_preferences = Preferences(user=user, movie=movie, score=score         # TODO:  what params?
-                                    # id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-                                    # user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-                                    # preferred_app_lang = db.Column(db.String(50))
-                                    # default_subtitle = db.Column(db.String)
-                                    # default_dubbing = db.Column(db.String)
-                                    # default_genre = db.Column(db.String)
-                                    # default_release_date = db.Column(db.String(20))
-                                    # default_duration = db.Column(db.Integer)
-                                    )                   
+    user_preferences = Preferences(user_id = db.Column(db.Integer, 
+            db.ForeignKey(User.query.get(id))),
+        query_run_date_time = datetime.datetime.now(),
+        query_string = query_string,
+        payload = payload,
+        param_subtitle = param_subtitle,
+        param_dubbing = param_dubbing,
+        param_genre = param_genre,
+        param_release_date = param_release_date,
+        param_duration = param_duration,
+        param_total_seasons = param_total_seasons
+        )                          
 
     db.session.add(user_preferences)
     db.session.commit()
@@ -165,88 +169,90 @@ def add_user_preference_to_preferences():                                       
     return user_preferences
 
 
-def get_user_preferences_specific_kvp():
-    """Return ONE SPECIFIC default preference of current User's preferences"""
+# def get_user_preferences_specific_kvp():
+#     """Return ONE SPECIFIC default preference of current User's preferences"""
 
-    pass                                                                        # TODO:  get only one value out of a specified col in the most recent row
-
-
-def get_user_preferences_current_collection():
-    """Return the most recent collection of this User's default preferences."""
-
-    pass                                                                        # TODO:  get only the most recent entry in the table, like, the largest ID?
+#     pass                                                                      # TODO:  get only one value out of a specified col in the most recent row
 
 
-def get_user_preferences_all_time():
+# def get_user_preferences_current_collection():
+#     """Return the most recent collection of this User's default preferences."""
+
+#     pass                                                                      # TODO:  get only the most recent entry in the table, like, the largest ID?
+
+
+def get_user_preferences_all_time(user):
     """Return all of the collections of this User's default preferences from forever ever."""
 
-    return preferences.query.filter(User.query.get(id))                         # TODO:  get only the most recent entry in the table, like, the largest ID?
+    return Preferences.query.filter(User.query.get(id))                        
 
 
 #*############################################################################*#
 #*#                            QUERY OPERATIONS                              #*#
 #*############################################################################*#
 
-def search_specific_movie():                                                    # TODO:  what params?
-    """Allow user to search for content based on specific input parameters      # ? is there a better, more general term than "movie", TV shows count, too
+# def search_specific_movie():                                                    # TODO:  what params?
+#     """Allow user to search for content based on specific input parameters      # ? is there a better, more general term than "movie", TV shows count, too
     
-    """                                                                         # TODO:  update docstring with doctests      
+#     """                                                                         # TODO:  update docstring with doctests      
 
-    pass                                                                        # TODO:  complete function stub 
-
-
-def get_single_result_by_id(movie_id):
-    """Return a single movie by primary key."""
-
-    pass                                                                        # TODO:  complete function stub 
+#     pass                                                                        # TODO:  complete function stub 
 
 
-def search_random_top_3_results():                                                 # TODO:  what params?
-    """Allow user to search for top 3 randomly selected results
+# def get_single_result_by_id(movie_id):
+#     """Return a single movie by primary key."""
+
+#     pass                                                                        # TODO:  complete function stub 
+
+
+# def search_random_top_3_results():                                                 # TODO:  what params?
+#     """Allow user to search for top 3 randomly selected results
     
-    Should return top ranked result based on both:
-        1.) user default preferences (if any)
-        2.) content's avg review score ( sort by --> decending )
-        3.) if/when score is tied, then randomize to select only 3 
+#     Should return top ranked result based on both:
+#         1.) user default preferences (if any)
+#         2.) content's avg review score ( sort by --> decending )
+#         3.) if/when score is tied, then randomize to select only 3 
 
-    """                                                                         # TODO:  update docstring with doctests 
+#     """                                                                         # TODO:  update docstring with doctests 
 
-    pass                                                                        # TODO:  complete function stub 
+#     pass                                                                        # TODO:  complete function stub 
 
 
-def search_x_movies_based_on_y_preferences():                                          # TODO:  what params?
-    """Allow user to search for top X number of results 
+# def search_x_movies_based_on_y_preferences():                                          # TODO:  what params?
+#     """Allow user to search for top X number of results 
     
-    Should return top ranked result based on both:
-        1.) user default preferences (if any)
-        2.) content's avg review score ( sort by --> decending )
-        3.) if/when score is tied, then randomize to select only top 10
-        4.) returning X qty selected by user
+#     Should return top ranked result based on both:
+#         1.) user default preferences (if any)
+#         2.) content's avg review score ( sort by --> decending )
+#         3.) if/when score is tied, then randomize to select only top 10
+#         4.) returning X qty selected by user
 
-    """                                                                         # TODO:  update docstring with doctests 
+#     """                                                                         # TODO:  update docstring with doctests 
 
-    pass                                                                        # TODO:  complete function stub 
+#     pass                                                                        # TODO:  complete function stub 
 
 
 #*############################################################################*#
 #*#                        QUERYHISTORY OPERATIONS                           #*#
 #*############################################################################*#
 
-def add_query_to_query_history():                                               # TODO:  what params?
-    """Create a new entry in Query History with query results"""
+def add_query_to_query_history(user, query_string, 
+    payload, param_subtitle="", param_dubbing="", param_genre="", 
+    param_release_date="", param_duration="", param_total_seasons=""):          # TODO:  what params?
+    """Create a new entry in Query History with query results                   # TODO: update docstring with doctest
+    """
 
-    query = QueryHistory(user=user, movie=movie, score=score                    # TODO:  what params?
-                        # user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-                        # query_run_date_time = db.Column(db.DateTime)
-                        # query_string = db.Column(db.String)
-                        # payload = db.Column(db.String)        
-                        # param_subtitle = db.Column(db.String)
-                        # param_dubbing = db.Column(db.String)
-                        # param_genre = db.Column(db.String)
-                        # param_release_date = db.Column(db.String(20))
-                        # param_duration = db.Column(db.Integer)
-                        # param_total_seasons = db.Column(db.String(20))
-                        )                   
+    query = QueryHistory(user_id = (User.query.get(id)),
+        query_run_date_time = datetime.datetime.now(),
+        query_string = query_string,
+        payload = payload,
+        param_subtitle = param_subtitle,
+        param_dubbing = param_dubbing,
+        param_genre = param_genre,
+        param_release_date = param_release_date,
+        param_duration = param_duration,
+        param_total_seasons = param_total_seasons
+        )                   
 
     db.session.add(query)
     db.session.commit()
@@ -254,16 +260,16 @@ def add_query_to_query_history():                                               
     return query
 
 
-def get_previous_query_from_history():                                          # TODO:  what params?
-    """Retreive only users's most recent search from the QueryHistory table"""  # TODO:  update docstring with doctests    
+# def get_previous_query_from_history():                                          # TODO:  what params?
+#     """Retreive only users's most recent search from the QueryHistory table"""  # TODO:  update docstring with doctests    
 
-    pass                                                                        # TODO:  complete function stub 
+#     pass                                                                        # TODO:  complete function stub 
 
 
-def get_all_query_history():                                                    # TODO:  what params?
+def get_all_query_history(user):                                                    # TODO:  what params?
     """Retreive all of a users's searches from the QueryHistory table"""        # TODO:  update docstring with doctests    
 
-    return queryhistory.query.all()                                             # TODO:  complete function stub 
+    return queryhistory.query.all(QueryHistory.user_id == (User.query.get(id))) # TODO:  complete function stub 
 
 
 if __name__ == '__main__':
